6a410cb1c9d8106e14ec235ba9418035eadbc293
Aliaksandr Hrakhouski <aliaksandr_hrakhouski@epam.com>
2023-04-10 20:48:24 +0200

    repo link added

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/username/reposotoryName";
+    public static String REPO_LINK = "https://github.com/saxavz/stage2-module1-gitpractice.git";
 }
